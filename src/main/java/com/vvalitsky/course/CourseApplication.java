ff0832f9670c02da8e2552638da07a2840e1592d
Vladislav Valitsky <vvalitsky>
2020-08-09 20:31:10 +0300

    Checkstyle fix

--- src/main/java/com/vvalitsky/course/CourseApplication.java
+++ src/main/java/com/vvalitsky/course/CourseApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CourseApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
 	}
